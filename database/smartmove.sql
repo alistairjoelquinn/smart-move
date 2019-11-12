40ed6157bca1b70ab51d19ccda3f59f1bc4d064d
alistairjoelquinn <alistairjoelquinn@gmail.com>
2019-11-12 20:19:20 +0100

    just for luck

--- database/smartmove.sql
+++ database/smartmove.sql
@@ -4,9 +4,9 @@ CREATE TABLE images (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL CHECK (name != ''),
     url VARCHAR(255) NOT NULL CHECK (url != ''),
-    modal1 VARCHAR(255) NOT NULL CHECK (url != ''),
-    modal2 VARCHAR(255) NOT NULL CHECK (url != ''),
-    audio VARCHAR(255) NOT NULL CHECK (url != '')
+    modal1 VARCHAR(255) NOT NULL CHECK (modal1 != ''),
+    modal2 VARCHAR(255) NOT NULL CHECK (modal2 != ''),
+    audio VARCHAR(255) NOT NULL CHECK (audio != '')
 );
 
 SELECT * FROM images;
